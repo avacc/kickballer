cf6941d34eb527370ed635794f832f67e308dfec
Alex Vaccarino <agvaccarino@gmail.com>
2013-01-22 21:53:46 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kickballer::Application.config.secret_token = 'd921e9dfddb30beeedd38018cf0b3a5558eeb5f5c6da88b7d665c9ff795b2a8eb9047dfacbb4b96ca7ddc1c3c8ded6bc604009a0396edfdf6d30745e93154cb0'
