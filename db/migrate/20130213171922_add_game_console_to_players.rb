813267fea03ce611b84d0514d912edb904b693db
Alex Vaccarino <agvaccarino@gmail.com>
2013-02-13 12:26:50 -0500

    Updated players migration file

--- db/migrate/20130213171922_add_game_console_to_players.rb
+++ db/migrate/20130213171922_add_game_console_to_players.rb
@@ -1,4 +1,10 @@
 class AddGameConsoleToPlayers < ActiveRecord::Migration
   def change
+    def self.up
+      add_column :players, :game_console, :string
+    end
+    def self.down
+      remove_column :players, :game_console
+    end
   end
 end
