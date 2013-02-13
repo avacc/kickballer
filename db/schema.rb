c3acb994632b64b5edb432f7718e67c4c949b26d
Alex Vaccarino <agvaccarino@gmail.com>
2013-02-13 12:45:02 -0500

    Update

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130207010830) do
+ActiveRecord::Schema.define(:version => 20130213173607) do
 
   create_table "players", :force => true do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.text     "email"
-    t.text     "gamer_tag"
     t.string   "preferred_class"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.string   "gamer_tag"
+    t.string   "email"
   end
 
   create_table "squads", :force => true do |t|
