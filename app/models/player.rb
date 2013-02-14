7e6623894abe9a18899cbff7fb26653cc6544291
Alex Vaccarino <agvaccarino@gmail.com>
2013-02-13 19:50:24 -0500

    update

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,4 @@
 class Player < ActiveRecord::Base
   belongs_to :squad
-  attr_accessible :email, :first_name, :gamer_tag, :last_name, :preferred_class
+  attr_accessible :email, :first_name, :gamer_tag, :last_name, :preferred_class, :squad_id
 end
