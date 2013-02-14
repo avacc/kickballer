70bbb78184901a453d63d617eadffb1a24f747ec
Alex Vaccarino <agvaccarino@gmail.com>
2013-02-13 20:52:28 -0500

    Added gender to players

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130214004042) do
+ActiveRecord::Schema.define(:version => 20130214014858) do
 
   create_table "players", :force => true do |t|
     t.string   "first_name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(:version => 20130214004042) do
     t.integer  "squad_id"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.string   "gender"
   end
 
   add_index "players", ["squad_id"], :name => "index_players_on_squad_id"
